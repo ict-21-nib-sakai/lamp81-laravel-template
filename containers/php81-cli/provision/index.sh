4670b1e9524348b1262b0cff339eb414ff288cd8
☃Kiyonor SAKAI☃ <ict21sakai@outlook.jp>
2021-11-17 19:11:23 +0900

    Docker PHP 8.1 Release Candidate 6 CLI - Docker コンテナの初回自動構築時に composer.json に沿って各種パッケージをインストールするためのシェルスクリプトを書いた。

--- containers/php81-cli/provision/index.sh
+++ containers/php81-cli/provision/index.sh
@@ -4,5 +4,6 @@ cd `dirname $0`
 
 provision-crontab.sh
 provision-supervisord.sh
+provision-laravel-framework.sh
 
 echo 'Configuration files have been placed. Restart the container. (eg) "docker restart CONTAINER_NAME".'
