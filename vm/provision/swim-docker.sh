886dc6279923ce12a21eee083bcda35fd794a4ca
☃Kiyonor SAKAI☃ <ict21sakai@outlook.jp>
2021-11-25 20:50:16 +0900

    vm/provision - Docker PHP 8.1 Apache   - 仕上げ用のスクリプトは root ユーザーで実行する。   - コンテナがほぼ出来上がっている状態で一般ユーザーで操作できなかったので。

--- vm/provision/swim-docker.sh
+++ vm/provision/swim-docker.sh
@@ -4,7 +4,7 @@ APP_PREFIX="my_app"
 CURRENT_DIRECTORY=`pwd`
 
 cd ${CURRENT_DIRECTORY}/../../containers
-docker exec ${APP_PREFIX}-php81_apache-1 index.sh
+docker exec --user root ${APP_PREFIX}-php81_apache-1 index.sh
 docker exec --user root ${APP_PREFIX}-php81_cli-1 index.sh
 docker-compose restart
 
