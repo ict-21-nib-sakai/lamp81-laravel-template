b1f102298fb6ff35a038e4d6167bf0ae9bde1b7c
Kiyonori SAKAI <ict21sakai@outlook.jp>
2022-02-11 20:24:07 +0900

    #6 PHP Apache コンテナの仕上げ用スクリプト index.sh をなくしたい - 不必要になった index.sh ファイルを削除。

--- containers/php81-cli/provision/index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-provision-crontab.sh
-provision-supervisord.sh
-provision-laravel-framework.sh
-
-echo 'Configuration files have been placed. Restart the container. (eg) "docker restart CONTAINER_NAME".'
