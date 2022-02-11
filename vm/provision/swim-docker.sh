be2124aa6311833bf367d451cab8cc3ef9158c4d
Kiyonori SAKAI <ict21sakai@outlook.jp>
2022-02-11 20:40:00 +0900

    #6 PHP Apache コンテナの仕上げ用スクリプト index.sh をなくしたい - コンテナ仕上げ用のスクリプトを調整。   - PHP-CLI コンテナから index.sh を取り除いたので同様に、スクリプトを変更した。   - 構文を IDE の指示にしたがって直しておいた。

--- vm/provision/swim-docker.sh
+++ vm/provision/swim-docker.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 APP_PREFIX="my_app"
-CURRENT_DIRECTORY=`pwd`
+CURRENT_DIRECTORY=$(pwd)
 
-cd ${CURRENT_DIRECTORY}/../../containers
+cd "${CURRENT_DIRECTORY}"/../../containers || exit
 docker exec --user root ${APP_PREFIX}-php81_apache-1 index.sh
-docker exec --user root ${APP_PREFIX}-php81_cli-1 index.sh
 docker-compose restart
 
-cd $CURRENT_DIRECTORY
+cd "${CURRENT_DIRECTORY}" || exit
