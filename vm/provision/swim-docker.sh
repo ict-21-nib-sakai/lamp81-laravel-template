1a364d53acf7bbc12aa8a9161a3475882daa14a0
Kiyonori SAKAI <ict21sakai@outlook.jp>
2022-02-12 17:14:37 +0900

    #13 PHP Apache コンテナの仕上げ用スクリプト index.sh をなくしたい - vagrant up, vagrant provision の最後に実行されるスクリプトを調整。   - おそらく、このスクリプト自体、要らないと思う。

--- vm/provision/swim-docker.sh
+++ vm/provision/swim-docker.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-APP_PREFIX="my_app"
 CURRENT_DIRECTORY=$(pwd)
 
 cd "${CURRENT_DIRECTORY}"/../../containers || exit
-docker exec --user root ${APP_PREFIX}-php81_apache-1 index.sh
 docker-compose restart
 
 cd "${CURRENT_DIRECTORY}" || exit
